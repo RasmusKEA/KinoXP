800073bb4036792d7fcd6eb4cad1192a6eaf14f6
unknown <rasm937k@stud.kea.dk>
2021-03-25 22:44:51 +0100

    test#

--- src/main/java/com/example/demo/bootstrap/BootStrapData.java
+++ src/main/java/com/example/demo/bootstrap/BootStrapData.java
@@ -63,11 +63,9 @@ public class BootStrapData implements CommandLineRunner {
         bookingRepository.save(booking1);
         bookingRepository.save(booking);
 
-
-
         Movie movie = new Movie();
         movie.setMovieTitle("Rejsen til Saturn");
-        movie.setGenre("Komedie");
+        movie.setGenre("Documentary");
         movie.setReleaseYear(2006);
         movie.setImage("https://m.media-amazon.com/images/M/MV5BNDQ2NjIzNDMtMGZjOS00ZGIwLThiYTktM2JiN2E4MTIyZTdhXkEyXkFqcGdeQXVyNjY0ODg0MTA@._V1_.jpg");
         movie.setHall("Small hall");
@@ -76,7 +74,7 @@ public class BootStrapData implements CommandLineRunner {
 
         Movie movie1 = new Movie();
         movie1.setMovieTitle("Godzilla vs Kong");
-        movie1.setGenre("Dokumentar");
+        movie1.setGenre("Documentary");
         movie1.setReleaseYear(2006);
         movie1.setImage("https://upload.wikimedia.org/wikipedia/en/6/63/Godzilla_vs._Kong.png");
         movie1.setHall("Large hall");
@@ -85,7 +83,7 @@ public class BootStrapData implements CommandLineRunner {
 
         Movie movie2 = new Movie();
         movie2.setMovieTitle("Terkel i Knibe");
-        movie2.setGenre("Komedie");
+        movie2.setGenre("Comedy");
         movie2.setReleaseYear(2006);
         movie2.setImage("https://prod.cdn.bbaws.net/TDC_Blockbuster_-_Production/84/396/EG-273_po-reg-medium_orig-1576227034165.jpg");
         movie2.setHall("Small hall");
